679bbb78a8f945eb9210a673ef6cae666db54eee
Anibal Copitan <asistemas.ksanchez@gmail.com>
2015-02-24 12:06:06 -0500

    validation for enter to level's

---
+++ src/assets/js/function.js
@@ -0,0 +1,12 @@
+/* 
+ * helper Usefull functions by development
+ */
+
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
