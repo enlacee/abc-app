afc1e55ab8ed3d0f11badfae3943a4ffe064f83a
Anibal Copitan <acopitan@gmail.com>
2015-03-10 19:01:23 -0500

    up
    Signed-off-by: Anibal Copitan <acopitan@gmail.com>

---
+++ src/assets/js/1_27.js
@@ -0,0 +1,28 @@
+document.addEventListener('deviceready', onDeviceReady(URI.data), false);
+
+function onDeviceReady(data) {
+    //document.querySelector("#playMp3").addEventListener("load", playMP3, false);
+    var soundMp3 = '';
+    if (soundMp3.length > 0) {
+        playMP3(soundMp3);
+    } else {
+        console.log('no sound!');
+    }
+};
+
+function playMP3(myPathFileMp3) {
+    var mp3URL = getMediaURL(myPathFileMp3);
+    var media = new Media(mp3URL, null, mediaError);
+    media.play();
+}
+
+function getMediaURL(s) {
+    if(device.platform.toLowerCase() === "android") return "/android_asset/www/" + s;
+    return s;
+}
+
+function mediaError(e) {
+    alert('Media Error');
+    alert(JSON.stringify(e));
+}
+
