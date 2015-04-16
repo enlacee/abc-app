c6c86e640901d0933b2c0bce1b3edbbb9b914096
Anibal Copitan <acopitan@gmail.com>
2015-04-16 13:46:24 -0500

    update glosario : wapití

--- www/assets/js/glosario.js
+++ www/assets/js/glosario.js
@@ -205,207 +205,207 @@ var abc = [
 {
     name : 'MARIPOSA',
     image : imgBase +'m/3.png',
-    description : ''
+    description : 'Los lepidópteros (Lepidoptera, del griego «lepis», escama, y «pteron», ala) son un orden de insectos holometábolos, casi siempre voladores, conocidos comúnmente como mariposas; las más conocidas son las mariposas diurnas, pero la mayoría de las especies son nocturnas.'
 },
 {
     name : 'NUBE',
     image : imgBase +'n/1.png',
-    description : ''
+    description : 'Una nube es un hidrometeoro que consiste en una masa visible formada por cristales de nieve o gotas de agua microscópicas suspendidas en la atmósfera. Las nubes dispersan toda la luz visible y por eso se ven blancas.'
 },
 {
     name : 'NUTRIA',
     image : imgBase +'n/2.png',
-    description : ''
+    description : 'Los lutrinos (Lutrinae), conocidos comúnmente como nutrias, son una subfamilia de mamíferos carnívoros de la gran familia Mustelidae. Existen 13 especies de nutrias repartidas en 7 géneros, con una distribución de la población prácticamente mundial.'
 },
 {
     name : 'NEVADO',
     image : imgBase +'n/3.png',
-    description : ''
+    description : 'Que está cubierto de nieve paisaje nevado. Montaña cubierta de nieves perpetuas.'
 },
 {
     name : 'ÑU',
     image : imgBase +'n1/1.png',
-    description : ''
+    description : 'Connochaetes es un género de mamíferos artiodáctilos de la subfamilia Alcelaphinae. Son antílopes de África que incluyen al ñu negro (Connochaetes gnou) y al ñu azul (Connochaetes taurinus) como sus únicas especies. '
 },
 {
     name : 'ÑANDU',
     image : imgBase +'n1/2.png',
-    description : ''
+    description : 'Los ñandúes (Rhea) son aves paleognatas endémicas de Sudamérica, existiendo dos especies.1 No obstante, algunos autores clasifican al ñandú de Darwin (Rhea pennata) como Pterocnemia pennata.'
 },
 {
     name : 'ÑUSTA',
     image : imgBase +'n1/3.png',
-    description : ''
+    description : 'Ñusta era el nombre quechua para las REINAS O PRINCESA s en el Imperio inca.'
 },
 {
     name : 'OSO',
     image : imgBase +'o/1.png',
-    description : ''
+    description : 'Los úrsidos (Ursidae) son una familia de mamíferos carnívoros conocidos comúnmente como osos. Son animales de gran tamaño, generalmente omnívoros ya que, a pesar de su temible dentadura, comen frutos, raíces e insectos, además de carne'
 },
 {
     name : 'OJOS',
     image : imgBase +'o/2.png',
-    description : ''
+    description : 'Los ojos son órganos visuales que detectan la luz y la convierten en impulsos electroquímicos que viajan a través de neuronas. La célula fotoreceptora más simple de la visión consciente asocia la luz al movimiento.'
 },
 {
     name : 'OÍDOS',
     image : imgBase +'o/3.png',
-    description : ''
+    description : 'El oído es un conjunto de órganos cuyas funciones principales son dotar de equilibrio y audición al cuerpo de los humanos o animales.'
 },
 {
     name : 'PUMA',
     image : imgBase +'p/1.png',
-    description : ''
+    description : 'El puma, león de montaña, león o pantera (Puma concolor), es un mamífero carnívoro de la familia Felidae nativo de América. Este gran felino vive en más lugares que cualquier otro mamífero salvaje terrestre.'
 },
 {
     name : 'PAPAGAYO',
     image : imgBase +'p/2.png',
-    description : ''
+    description : 'Psittacoidea es una de las tres superfamilias del orden Psittaciformes. Contiene unas 330 especies de psitacoideos o loros típicos.'
 },
 {
     name : 'PINGÜINO',
     image : imgBase +'p/3.png',
-    description : ''
+    description : 'Los pingüinos (familia Spheniscidae, orden Sphenisciformes) son un grupo de aves marinas, no voladoras, que se distribuyen únicamente en el Hemisferio Sur, sobre todo en sus altas latitudes.'
 },
 {
     name : 'QUESO',
     image : imgBase +'q/1.png',
-    description : ''
+    description : 'El queso es un alimento sólido elaborado a partir de la leche cuajada de vaca, cabra, oveja, búfala, camello u otros mamíferos rumiantes. La leche es inducida a cuajarse usando una combinación de cuajo (o algún sustituto) y acidificación.'
 },
 {
     name : 'QUENA',
     image : imgBase +'q/2.png',
-    description : ''
+    description : 'La quena (en quechua: qina) es un instrumento de viento de bisel, usado de modo tradicional por los habitantes de los Andes centrales. La quena es tradicionalmente de caña o madera y tiene un total de siete agujeros, seis al frente y uno atrás, para el pulgar.'
 },
 {
     name : 'QUÍMICA',
     image : imgBase +'q/3.png',
-    description : ''
+    description : 'Es la ciencia que estudia tanto la composición, estructura y propiedades de la materia como los cambios que ésta experimenta durante las reacciones químicas y su relación con la energía.'
 },
 {
     name : 'RATÓN',
     image : imgBase +'r/1.png',
-    description : ''
+    description : 'Mus es un género de roedores miomorfos de la familia Muridae que incluye la mayoría de los roedores llamados comúnmente ratones, si bien el nombre de ratón se usa para varias especies más pertenecientes a otros géneros.'
 },
 {
     name : 'RANA',
     image : imgBase +'r/2.png',
-    description : ''
+    description : 'Los anuros son un grupo de anfibios, con rango taxonómico de orden, conocidos vulgarmente como ranas y sapos.'
 },
 {
     name : 'RUEDA',
     image : imgBase +'r/3.png',
-    description : ''
+    description : 'La rueda es una pieza mecánica circular que gira alrededor de un eje. Puede ser considerada una máquina simple, y forma parte del conjunto denominado elementos de máquinas.'
 },
 {
     name : 'SOL',
     image : imgBase +'s/1.png',
-    description : ''
+    description : 'Es una estrella del tipo espectral G2 que se encuentra en el centro del Sistema Solar y constituye la mayor fuente de radiación electromagnética de este sistema planetario.'
 },
 {
     name : 'SOFÁ',
     image : imgBase +'s/2.png',
-    description : ''
+    description : 'Un sofá es un mueble utilizado para sentarse confortablemente más de una persona. Se encuentran generalmente en el recibidor o el salón y/o la sala de la casa.'
 },
 {
     name : 'SILLA',
     image : imgBase +'s/3.png',
-    description : ''
+    description : 'La silla es un mueble cuya finalidad es servir de asiento a una persona. Suele tener cuatro patas, aunque puede haber de una, dos, tres o más.'
 },
 {
     name : 'TOMATE',
     image : imgBase +'t/1.png',
-    description : ''
+    description : 'Solanum lycopersicum, conocido comúnmente como jitomate, tomate o tomatera. Es una especie de la familia de las solanáceas originaria de América (Perú o México) y cultivada en todo el mundo.'
 },
 {
     name : 'TITÍ',
     image : imgBase +'t/2.png',
-    description : ''
+    description : 'Tití es el nombre vulgar de diferentes especies de primates americanos (platirrinos)'
 },
 {
     name : 'TUCÁN',
     image : imgBase +'t/3.png',
-    description : ''
+    description : 'Los ranfástidos (Ramphastidae), conocidos comúnmente como tucanes, diotedé, o diostedé, son una familia de aves piciformes que se caracterizan por poseer un pico muy desarrollado y de vivos colores. Miden entre 18 y 63 cm.'
 },
 {
     name : 'UVA',
     image : imgBase +'u/1.png',
-    description : ''
+    description : 'La uva es una fruta obtenida de la vid. Las uvas vienen en racimos y son pequeñas y dulces. Se comen frescas o se utilizan para producir agraz, mosto, vino y vinagre.'
 },
 {
     name : 'UÑA',
     image : imgBase +'u/2.png',
-    description : ''
+    description : 'La uña es una estructura anexa de la piel localizada en las regiones distales de los miembros. Se conoce también como garra y pezuña en los animales.'
 },
 {
     name : 'UNICORNIO',
     image : imgBase +'u/3.png',
-    description : ''
+    description : 'El unicornio es una criatura mitológica representada habitualmente como un caballo blanco con patas de antílope, barba de chivo y un cuerno en la frente.'
 },
 {
     name : 'VACA',
     image : imgBase +'v/1.png',
-    description : ''
+    description : 'La vaca, en el caso de la hembra, o toro en el caso del macho, es un mamífero artiodáctilo de la familia de los bóvidos'
 },
 {
     name : 'VENADO',
     image : imgBase +'v/2.png',
-    description : ''
+    description : 'Los cérvidos (Cervidae) son una familia de mamíferos rumiantes que incluye los ciervos o venados. Su tamaño es variable, siendo el alce el mayor (hasta 450 kg)'
 },
 {
     name : 'VOLCÁN',
     image : imgBase +'v/3.png',
-    description : ''
+    description : 'Un volcán (del nombre del dios mitológico romano Vulcano) es una estructura geológica por la que emergen magma en forma de lava, ceniza volcánica y gases provenientes del interior de la Tierra.'
 },
 {
     name : 'WATERPOLO',
     image : imgBase +'w/1.png',
-    description : ''
+    description : 'El waterpolo o polo acuático es un deporte que se practica en una piscina, en la cual se enfrentan dos equipos. El objetivo del juego es marcar el mayor número de goles en la portería del equipo contrario, durante el tiempo que dura el partido.'
 },
 {
-    name : 'WAPITI',
+    name : 'WAPITÍ',
     image : imgBase +'w/2.png',
-    description : ''
+    description : 'El uapití, wapití o ciervo canadiense (Cervus canadensis) es una especie de mamífero artiodáctilo de la familia de los cérvidos que vive en América del Norte, Asia central y oriental.'
 },
 {
     name : 'WINDSURF',
     image : imgBase +'w/3.png',
-    description : ''
+    description : 'El surf a vela, windsurf o tabla a vela es una modalidad del deporte a vela que consiste en desplazarse en el agua sobre una tabla algo similar a una de surf, provista de una vela.'
 },
 {
     name : 'XILÓFONO',
     image : imgBase +'x/1.png',
-    description : ''
+    description : 'Es un instrumento musical de percusión.'
 },
 {
     name : 'YOYO',
     image : imgBase +'y/1.png',
-    description : ''
+    description : 'El yo-yo es un juguete formado por un disco de madera, de plástico o de otros materiales con una ranura profunda en el centro de todo el borde, alrededor de la cual se enrolla un cordón que, anudado a un dedo se hace subir y bajar alternativamente.'
 },
 {
     name : 'YEGUA',
     image : imgBase +'y/2.png',
-    description : ''
+    description : 'El caballo (Equus ferus caballus) es un mamífero perisodáctilo domesticado de la familia de los équidos. Es un herbívoro solípedo de gran porte, cuello largo y arqueado, poblado por largas crines.'
 },
 {
     name : 'YACARÉ',
     image : imgBase +'y/3.png',
-    description : ''
+    description : 'Caiman es un género de crocodilios de la familia de los aligatóridos, conocidos vulgarmente como yacarés o caimanes.'
 },
 {
     name : 'ZORRO',
     image : imgBase +'z/1.png',
-    description : ''
+    description : 'Los vulpinos (Vulpini) son una tribu de mamíferos carnívoros incluidos en la familia de los cánidos. Se conocen comúnmente como zorros, zorras, raposos o raposas.'
 },
 {
     name : 'ZAPATO',
     image : imgBase +'z/2.png',
-    description : ''
+    description : 'El zapato es un accesorio de esemen de caballolla vestimenta hecho con la intención de proveer protección y comodidad al pie mientras realiza actividades varias. '
 },
 {
     name : 'ZAMPOÑA',
     image : imgBase +'z/3.png',
-    description : ''
+    description : 'La zampoña es un instrumento de viento compuesto de varios tubos ahuecados por un extremo y cerrados por el otro, dispuestos en forma vertical en una o dos hileras.'
 },
 ];
 
